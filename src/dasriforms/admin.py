2868fb5e57d6ac491aefb128f07f46de1f044de9
Laurent Paoletti <lp@providenz.fr>
2020-11-05 18:02:39 +0100

    Add form main fields

--- src/dasriforms/admin.py
+++ src/dasriforms/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DasriForm, WasteIdentifier, WastePackaging
+
+
+class WastePackagingInline(admin.TabularInline):
+    model = WastePackaging
+
+
+class WasteIdentifierInline(admin.TabularInline):
+    model = WasteIdentifier
+
+
+@admin.register(DasriForm)
+class DasriFormAdmin(admin.ModelAdmin):
+    list_display = ["id", "readableId"]
+    inlines = [WastePackagingInline, WasteIdentifierInline]
