c7ba30c88836ce9066c3d360a1450ba847918d3d
Laurent Paoletti <lp@providenz.fr>
2020-12-07 21:02:15 +0100

    Check permissions on dasri creation mutation

--- back/src/dasris/resolvers/mutations/dasriCreateForm.ts
+++ back/src/dasris/resolvers/mutations/dasriCreateForm.ts
@@ -7,27 +7,24 @@ import { GraphQLContext } from "../../../types";
 import {
   expandDasriFormFromDb,
   flattenDasriFormInput
-} from "../../form-converter";
+} from "../../dasri-converter";
 import { getReadableId } from "../../dasri-readable-id";
+import { checkIsAuthenticated } from "../../../common/permissions";
 
 const dasriCreateResolver = async (
   parent: ResolversParentTypes["Mutation"],
   { dasriCreateInput }: MutationDasriCreateArgs,
   context: GraphQLContext
 ) => {
-  //   const user = checkIsAuthenticated(context);
+  const user = checkIsAuthenticated(context);
 
   const flattenedInput = flattenDasriFormInput(dasriCreateInput);
 
-  const owner = await prisma.user.findOne({
-    where: { email: "lp@providenz.fr" }
-  });
-  console.log(flattenedInput);
-  const newForm = await prisma.dasriForm.create({
+  const newForm = await prisma.dasri.create({
     data: {
       ...flattenedInput,
       readableId: await getReadableId(),
-      owner: { connect: { id: owner?.id } }
+      owner: { connect: { id: user?.id } }
     }
   });
 
