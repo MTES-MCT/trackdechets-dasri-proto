5efe7a9e1fac43345006a9032c7908b2cb9022ea
Laurent Paoletti <lp@providenz.fr>
2020-12-07 21:01:42 +0100

    Update database util

--- back/src/dasris/database.ts
+++ back/src/dasris/database.ts
@@ -1,22 +1,51 @@
 import prisma from "../prisma";
 
-import { FormNotFound } from "./errors";
+import { DasriNotFound } from "./errors";
 import { UserInputError } from "apollo-server-express";
+import { DasriWhereInput, DasriWhereUniqueInput } from "@prisma/client";
 
-import { DasriForm } from "../generated/graphql/types";
+import { DasriRole } from "../generated/graphql/types";
 
 /**
  * Retrieves a form by id or readableId or throw a FormNotFound error
  */
-export async function getDasriOrDasriNotFound({ id, readableId }: DasriForm) {
+export async function getDasriOrDasriNotFound({
+  id,
+  readableId
+}: DasriWhereUniqueInput) {
   if (!id && !readableId) {
     throw new UserInputError("You should specify an id or a readableId");
   }
-  const form = await prisma.dasriForm.findOne({
+  const form = await prisma.dasri.findOne({
     where: { id: id ? id : readableId }
   });
   if (form == null || form.isDeleted == true) {
-    throw new FormNotFound(id ? id.toString() : readableId);
+    throw new DasriNotFound(id ? id.toString() : readableId);
   }
   return form;
+}
+
+/**
+ * Get a filter to retrieve dasris the passed siret has rights on
+ * Optional parameter roles allows to filter on specific roles
+ * For example getDasrisRightFilter(company, [TRANSPORTER]) will return a filter
+ * only for the forms in which the company appears as a transporter
+ * @param siret the siret to filter on
+ * @param roles optional [FormRole] to refine filter
+ */
+export function getDasrisRightFilter(siret: string, roles?: DasriRole[]) {
+  const filtersByRole: { [key in DasriRole]: Partial<DasriWhereInput>[] } = {
+    ["RECIPIENT"]: [{ recipientCompanySiret: siret }],
+    ["EMITTER"]: [{ emitterCompanySiret: siret }],
+    ["TRANSPORTER"]: [{ transporterCompanySiret: siret }]
+  };
+
+  return {
+    OR: Object.keys(filtersByRole)
+      .filter((role: DasriRole) =>
+        roles?.length > 0 ? roles.includes(role) : true
+      )
+      .map(role => filtersByRole[role])
+      .flat()
+  };
 }
