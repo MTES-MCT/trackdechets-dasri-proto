65d8fab639f36a5ca1b83f65851863c99a1eb7f6
Laurent Paoletti <lp@providenz.fr>
2020-12-07 09:21:34 +0100

    Integrate dasri rsolvers to main schema

--- back/src/schema.ts
+++ back/src/schema.ts
@@ -4,11 +4,12 @@ import scalarResolvers from "./scalars";
 import companiesResolvers from "./companies/resolvers";
 import usersResolvers from "./users/resolvers";
 import formsResolvers from "./forms/resolvers";
+import dasriResolvers from "./dasris/resolvers";
 
 // Merge GraphQL schema by merging types, resolvers and shields
 // definitions from differents modules
 
-const repositories = ["users", "companies", "forms"];
+const repositories = ["users", "companies", "forms", "dasris"];
 
 const typeDefsPath = repositories.map(
   repository => `${__dirname}/${repository}/*.graphql`
@@ -22,7 +23,8 @@ const resolvers = [
   scalarResolvers,
   companiesResolvers,
   formsResolvers,
-  usersResolvers
+  usersResolvers,
+  dasriResolvers
 ];
 
 export { typeDefs, resolvers };
