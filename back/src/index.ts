5be63101e5150f4418c4257cd1ce2ed2930f3fc2
Laurent Paoletti <lp@providenz.fr>
2020-12-09 16:23:57 +0100

    Fix localhost

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,7 +2,7 @@ import { initSubscriptions } from "./events";
 import { app } from "./server";
 
 const port = process.env.BACK_PORT || 80;
-const backHost = process.env.BACK_HOST || "121.0.0.1";
+const backHost = process.env.BACK_HOST || "127.0.0.1";
 app.listen({ host: backHost, port: port }, () =>
   console.info(`Server is running on port ${port}`)
 );
