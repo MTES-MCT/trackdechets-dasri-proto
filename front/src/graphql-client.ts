a8187574bbc9e48a51628274f5e6dc3cb49818ed
Laurent Paoletti <lp@providenz.fr>
2020-12-15 19:30:11 +0100

    Port typename fix

--- front/src/graphql-client.ts
+++ front/src/graphql-client.ts
@@ -1,7 +1,33 @@
-import { InMemoryCache, ApolloClient } from "@apollo/client";
+import {
+  InMemoryCache,
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+} from "@apollo/client";
+
+/**
+ * Automatically erase `__typename` from variables
+ * This enable devs to use objects fetched from the server
+ * and not worry about `__typename` potentially breaking a mutation
+ */
+const cleanTypeNameLink = new ApolloLink((operation, forward) => {
+  if (operation.variables) {
+    const omitTypename = (key, value) =>
+      key === "__typename" ? undefined : value;
+    operation.variables = JSON.parse(
+      JSON.stringify(operation.variables),
+      omitTypename
+    );
+  }
+  return forward(operation);
+});
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_ENDPOINT,
+  credentials: "include",
+});
 
 export default new ApolloClient({
-  uri: process.env.REACT_APP_API_ENDPOINT,
   cache: new InMemoryCache(),
-  credentials: "include",
+  link: ApolloLink.from([cleanTypeNameLink, httpLink]),
 });
