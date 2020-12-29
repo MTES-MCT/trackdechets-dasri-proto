6cdd9a6478f265dc372274306f2a63551a57ddd2
Laurent Paoletti <lp@providenz.fr>
2020-12-29 11:39:27 +0100

    Revert hardcoded settings

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./server";
 
 const port = process.env.BACK_PORT || 80;
 const backHost = process.env.BACK_HOST || "127.0.0.1";
-app.listen({ host: "0.0.0.0", port: 8080 }, () =>
+app.listen({ host: backHost, port: port }, () =>
   console.info(`Server is running on port ${port}`)
 );
 
