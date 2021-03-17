8ade60a452e5c51eeeb78e5f0df1dcaaf3a70c17
Laurent Paoletti <lp@providenz.fr>
2021-03-17 22:42:39 +0100

    Default host

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,9 +2,7 @@ import { initSubscriptions } from "./events";
 import { app } from "./server";
 
 const port = process.env.BACK_PORT || 80;
-const backHost = process.env.BACK_HOST || "127.0.0.1";
-app.listen({ host: backHost, port: port }, () =>
-  console.info(`Server is running on port ${port}`)
-);
+
+app.listen(port, () => console.info(`Server is running on port ${port}`));
 
 initSubscriptions();
